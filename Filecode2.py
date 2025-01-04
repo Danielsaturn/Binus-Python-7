b61b322a9ec8efde522e0b1454bcd74e852418bb
Danielsaturn <danielbussiness2000@gmail.com>
2025-01-04 15:26:14 +0700

    Create Filecode2.py

---
+++ Filecode2.py
@@ -0,0 +1,43 @@
+def writefile():
+    filename = input("Please enter the file name: ")
+    inputName = input("Please enter your name: ")
+    inputAge = input("Please enter your age: ")
+    inputAdress = input("Please enter your adress: ")
+    inputEmail = input("Please input your email: ")
+
+    filewrite = open(filename +".txt", "w")
+    filewrite.write("FileName: " + filename + "\nName: " + inputName + "\nAge: " + inputAge + "\nAdress: " + inputAdress + "\nEmail: " + inputEmail)
+    filewrite.close()
+
+def readfile():
+    filename = input("Please enter the file name: ")
+    fileread = open(filename + ".txt", "r")
+    text= fileread.read()
+    print("This is the data that was saved")
+    print(text)
+    fileread.close()
+
+while(True):
+  print("""===== OOP Program =====
+    1. Membuat file
+    2. Membaca file
+    3. Menambah file
+    4. Keluar Dari Program""")
+  numbermenu = input("Please enter the number in accordance to what options that are available above: ")
+
+  if (numbermenu == "1"):
+
+    writefile()
+    print("The file has been successfully saved")
+
+  elif(numbermenu == "2"):
+      print("\n")
+      readfile()
+
+  elif(numbermenu == "3"):
+
+    writefile()
+    print("The file has been successfully saved")
+
+  else:
+      print("Please enter the correct number!")
